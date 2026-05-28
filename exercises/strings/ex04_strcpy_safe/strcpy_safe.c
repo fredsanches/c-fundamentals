b3a849c1cade0662c8be008eac80fc91796792d1
Frederico Sanches <fredsparanhos@gmail.com>
2026-05-28 09:14:50 -0300

    add strings ex04 solution.

--- exercises/strings/ex04_strcpy_safe/strcpy_safe.c
+++ exercises/strings/ex04_strcpy_safe/strcpy_safe.c
@@ -5,16 +5,20 @@ int my_strcpy_safe(char[] , size_t, const char[]);
 
 int my_strcpy_safe(char dest[], size_t dest_capacity, const char src[])
 {
-  
-
-  /*
-   * TODO:
-   * 1. Decide what to do when dest_capacity is 0.
-   * 2. Copy at most the bytes that fit in dest.
-   * 3. Always account for the final '\0'.
-   * 4. Return 1 only when the whole source string was copied.
-   */
-  return 0;
+  if (dest_capacity == 0) {
+    return 0;
+  }
+  for (size_t i = 0; i < dest_capacity; i++)
+  {
+    dest[i] = src[i];
+    if (src[i] == '\0'){return 1;}
+    if (i == (dest_capacity - 1)){
+      dest[i] = '\0';
+      // truncated string -> failure
+      return 0;
+    }
+  }
+  return 1;
 }
 
 int main(void)
@@ -24,5 +28,29 @@ int main(void)
   printf("Implement my_strcpy_safe(), then test buffer capacity boundaries.\n");
   printf("buffer capacity: %zu bytes\n", sizeof(buffer));
 
+  char v1[] = "abcdefghijk";
+  my_strcpy_safe(buffer, sizeof(buffer), v1);
+  printf("buffer == %s", buffer);
+
   return 0;
 }
+
+/*
+ * Design Questions
+ * Who owns the destination memory?
+ * R: Main functions owns the destination memory.
+ * 
+ * How many bytes are available in `dest`?
+ * R: Same bytes available in buffer (dest_capacity).
+ * 
+ * How many bytes are needed to copy src, including '\0'?
+ * R: The buffer size must be grater than zero to copy at lesat '\0' char.
+ * R: If buffer size grater than src size, the entire data will be copied..
+ * 
+ * Where is the last valid index in dest?
+ * R: Is in dest[(dest_capacity - 1)] -> C is 0-indexed.
+ * 
+ * What should happen when src is an empty string?
+ * R: Only '\0' char will be copied. So `dest` will become an empty string too.
+
+*/
