8767db2808f2022391e00898d82f9c4108fa8286
Frederico Sanches <fredsparanhos@gmail.com>
2026-05-28 11:39:52 -0300

    add strings ex05 solution.

--- exercises/strings/ex05_strncpy_safe/strncpy_safe.c
+++ exercises/strings/ex05_strncpy_safe/strncpy_safe.c
@@ -1,29 +1,38 @@
 #include <stddef.h>
 #include <stdio.h>
 
-size_t my_strncpy_safe(char dest[], size_t dest_capacity, const char src[]);
+size_t my_strncpy_safe(char[], size_t dest_capacity, const char src[]);
 
 size_t my_strncpy_safe(char dest[], size_t dest_capacity, const char src[])
 {
-  (void)dest;
-  (void)dest_capacity;
-  (void)src;
-
-  /*
-   * TODO:
-   * Copy as many non-null characters as safely fit.
-   * Leave room for '\0' whenever dest_capacity is greater than 0.
-   * Return the number of non-null characters actually copied.
-   */
-  return 0;
+  if (dest_capacity == 0) {
+    return 0;
+  }
+  size_t count = 0;
+  for (size_t i = 0; i < dest_capacity; i++)
+  {
+    if (src[i] == '\0' || i == (dest_capacity - 1)){
+      dest[i] = '\0';
+      break;
+    }
+    dest[i] = src[i];
+    ++count;
+  }
+  // size without null terminator
+  return count;
 }
 
 int main(void)
 {
-  char buffer[5];
+  char buffer[1];
 
   printf("Implement my_strncpy_safe(), then test truncation behavior.\n");
   printf("buffer capacity: %zu bytes\n", sizeof(buffer));
+
+  char v1[] = "abcd";
+  size_t result = 0;
+  result = my_strncpy_safe(buffer, sizeof(buffer), v1);
+  printf("Number of non null chars: %zu\n", result);
 
   return 0;
 }
